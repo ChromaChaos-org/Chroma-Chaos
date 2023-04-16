17fb1489943ba1fcf0b55f165298834c1ac74dc7
fiona <fiona@LAPTOP-U7GO1IF3>
2023-04-16 13:58:53 +0800

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using System;
+
+namespace App3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        public void OnRandomizeButtonClicked(View view)
+        {
+            Random rand = new Random();
+            int randomColorInt = rand.Next(0x1000000);
+            int red = (randomColorInt & 0xFF0000) >> 16;
+            int green = (randomColorInt & 0xFF00) >> 8;
+            int blue = randomColorInt & 0xFF;
+            Color randomColor = Color.Rgb(red, green, blue);
+
+            view.SetBackgroundColor(randomColor);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
